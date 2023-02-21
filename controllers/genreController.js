afcae23d0b9200e20aba5ecb3a39e7e1ad080761
Lucas Santos <lucas.silva1203@outlook.com>
2023-02-21 18:08:56 -0300

    Create POST method to create new genres

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,6 +1,7 @@
 const Genre = require("../models/genre");
 const Book = require("../models/book");
-const async = require("async")
+const async = require("async");
+const { body, validationResult } = require("express-validator");
 
 exports.genre_list = (request, response, next) => {
     Genre.find({})
@@ -44,26 +45,57 @@ exports.genre_detail = (request, response, next) => {
     );
 };
 
-exports.genre_create_get = (request, response) => {
+exports.genre_create_get = (request, response, next) => {
+    response.render("genre_form", { title: "Create Genre" });
+};
+
+exports.genre_create_post = [
+    body("name", "Genre name required").trim().isLength({ min: 1 }).escape(),
+    (request, response, next) => {
+        const errors = validationResult(request);
+        const genre = new Genre({ name: request.body.name });
+
+        if (!errors.isEmpty()) {
+            response.render("genre_form", {
+                title: "Create Genre",
+                genre,
+                errors: errors.array(),
+            });
+            return;
+        } else {
+            Genre.findOne({ name: request.body.name }).exec(
+                (err, found_genre) => {
+                    if (err) {
+                        return next(err);
+                    }
+                    if(found_genre){
+                        response.redirect(found_genre.url)
+                    } else {
+                        genre.save((err) => {
+                            if (err) {
+                                return next(err)
+                            }
+                            response.redirect(genre.url)
+                        })
+                    }
+                }
+            );
+        }
+    },
+];
+
+exports.genre_delete_get = (request, response, next) => {
     response.send("Hold on, not yet");
 };
 
-exports.genre_create_post = (request, response) => {
+exports.genre_delete_post = (request, response, next) => {
     response.send("Hold on, not yet");
 };
 
-exports.genre_delete_get = (request, response) => {
+exports.genre_update_get = (request, response, next) => {
     response.send("Hold on, not yet");
 };
 
-exports.genre_delete_post = (request, response) => {
-    response.send("Hold on, not yet");
-};
-
-exports.genre_update_get = (request, response) => {
-    response.send("Hold on, not yet");
-};
-
-exports.genre_update_post = (request, response) => {
+exports.genre_update_post = (request, response, next) => {
     response.send("Hold on, not yet");
 };
