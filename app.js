a71ff9a040f1797f8335d139f297adf41957f27c
Lucas Santos <lucas.silva1203@outlook.com>
2023-02-21 13:55:23 -0300

    Add a new index and create functional queries

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let logger = require('morgan');
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
+const catalogRouter = require("./routes/catalog")
 
 let app = express();
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,33 +1,33 @@
 const Author = require("../models/author")
 
 exports.author_list = (request, response) => {
-  resizeBy.send("Hold on, not yet")
+  response.send("Hold on, not yet")
 }
 
 exports.author_detail = (request, response) => {
-  resizeBy.send("Hold on, not yet", request.params.id)
+  response.send("Hold on, not yet", request.params.id)
 }
 
 exports.author_create_get = (request, response) => {
-  resizeBy.send("Hold on, not yet")
+  response.send("Hold on, not yet")
 }
 
 exports.author_create_post = (request, response) => {
-  resizeBy.send("Hold on, not yet")
+  response.send("Hold on, not yet")
 }
 
 exports.author_delete_get = (request, response) => {
-  resizeBy.send("Hold on, not yet")
+  response.send("Hold on, not yet")
 }
 
 exports.author_delete_post = (request, response) => {
-  resizeBy.send("Hold on, not yet")
+  response.send("Hold on, not yet")
 }
 
 exports.author_update_get = (request, response) => {
-  resizeBy.send("Hold on, not yet")
+  response.send("Hold on, not yet")
 }
 
 exports.author_update_post = (request, response) => {
-  resizeBy.send("Hold on, not yet")
+  response.send("Hold on, not yet")
 }
